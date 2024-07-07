68c645214823f3fe82142c6c48cf7615f05a3cfa
Chris Loi <loichris97@gmail.com>
2024-07-06 21:42:16 -0700

    init EnrollmentView.js

--- src/components/instructor/EnrollmentsView.js
+++ src/components/instructor/EnrollmentsView.js
@@ -1,5 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { TextField } from '@mui/material';
+import { SERVER_URL } from '../../Constants';
 
 // instructor view list of students enrolled in a section 
 // use location to get section no passed from InstructorSectionsView
@@ -9,16 +11,70 @@ import {useLocation} from 'react-router-dom';
 //  grade column is an input field
 //  hint:  <input type="text" name="grade" value={e.grade} onChange={onGradeChange} />
 
-const EnrollmentsView = (props) => {
-
+const EnrollmentsView = () => {
     const location = useLocation();
-    const {secNo, courseId, secId} = location.state;
+    const { secNo } = location.state;
 
-    return(
-        <> 
-            <h3>Not implemented</h3>   
-        </>
+    const [enrollments, setEnrollments] = useState([]);
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const fetchEnrollments = async () => {
+            try {
+                const response = await fetch(`${SERVER_URL}/sections/${secNo}/enrollments`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setEnrollments(data);
+                } else {
+                    const json = await response.json();
+                    setMessage("response error: " + json.message);
+                }
+            } catch (err) {
+                setMessage("network error: " + err);
+            }
+        };
+        fetchEnrollments();
+    }, [secNo]);
+
+    const handleGradeChange = (e, enrollmentId) => {
+        const { value } = e.target;
+        setEnrollments(enrollments.map(en => en.enrollmentId === enrollmentId ? { ...en, grade: value } : en));
+    };
+
+    return (
+        <div>
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                <tr>
+                    <th>Enrollment ID</th>
+                    <th>Student ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Grade</th>
+                </tr>
+                </thead>
+                <tbody>
+                {enrollments.map(e => (
+                    <tr key={e.enrollmentId}>
+                        <td>{e.enrollmentId}</td>
+                        <td>{e.studentId}</td>
+                        <td>{e.name}</td>
+                        <td>{e.email}</td>
+                        <td>
+                            <TextField
+                                type="text"
+                                name="grade"
+                                value={e.grade}
+                                onChange={(ev) => handleGradeChange(ev, e.enrollmentId)}
+                            />
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
-}
+};
 
 export default EnrollmentsView;
