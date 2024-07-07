fb24093df2d0b17d282e80e2c88b7cc3c85f1ca6
Chris Loi <loichris97@gmail.com>
2024-07-06 21:41:33 -0700

    init AssignmentUpdate.js

--- src/components/instructor/AssignmentUpdate.js
+++ src/components/instructor/AssignmentUpdate.js
@@ -1,17 +1,69 @@
 import React, { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from '@mui/material';
+import { SERVER_URL } from '../../Constants';
 
 //  instructor updates assignment title, dueDate 
 //  use an mui Dialog
 //  issue PUT to URL  /assignments with updated assignment
 
-const AssignmentUpdate = (props)  => {
+const AssignmentUpdate = ({ assignment, open, handleClose }) => {
+    const [updatedAssignment, setUpdatedAssignment] = useState({ ...assignment });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUpdatedAssignment({ ...updatedAssignment, [name]: value });
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/assignments`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedAssignment),
+            });
+            if (response.ok) {
+                handleClose(true);
+            } else {
+                const json = await response.json();
+                console.error("response error: " + json.message);
+            }
+        } catch (err) {
+            console.error("network error: " + err);
+        }
+    };
 
     return (
-        <>
-          <h3>Not implemented</h3>  
-        </>                       
-    )
-}
+        <Dialog open={open} onClose={() => handleClose(false)}>
+            <DialogTitle>Update Assignment</DialogTitle>
+            <DialogContent>
+                <TextField
+                    margin="dense"
+                    label="Title"
+                    type="text"
+                    name="title"
+                    fullWidth
+                    value={updatedAssignment.title}
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="dense"
+                    label="Due Date"
+                    type="date"
+                    name="dueDate"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                    value={updatedAssignment.dueDate}
+                    onChange={handleChange}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => handleClose(false)}>Cancel</Button>
+                <Button onClick={handleSubmit}>Update</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
 
 export default AssignmentUpdate;
